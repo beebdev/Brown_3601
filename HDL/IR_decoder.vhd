6859bc894ec41da13cdc0b69ac163814c5811559
Elton Shih <z5187581@student.unsw.edu.au>
2020-09-29 13:34:29 +1000

    instantiated vhd entity files

---
+++ HDL/IR_decoder.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity IR_decoder is
+end IR_decoder;
+
+architecture Behavioral of IR_decoder is
+
+begin
+
+
+end Behavioral;
+
---
+++ HDL/clk_tree.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clk_tree is
+end clk_tree;
+
+architecture Behavioral of clk_tree is
+
+begin
+
+
+end Behavioral;
+
